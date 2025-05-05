2cfc2a02f44af28d12717160a68b20b63a6cd74d
Rafhael <rafhael.brum@gmail.com>
2025-05-05 13:52:33 -0300

    add url handler validator

--- src/controllers/postHandler.ts
+++ src/controllers/postHandler.ts
@@ -1,19 +1,21 @@
 import { Request, Response } from "express";
 import { createShortUrl } from "../models/postModel";
 import { shortUrlInput } from "../types/shortUrl";
+import validator from 'validator';
 
-export async function createShortUrlHandler(req: Request, res: Response) { // FALTA BOTAR UM VALIDATOR NA URL
+
+export async function createShortUrlHandler(req: Request, res: Response) {
     try {
         const data: shortUrlInput = req.body;
-        if (!data.url) {
-            res.status(400).json({ error: 'Invalid URL' });
+        if (!data.url || !validator.isURL(data.url)) {
+            return res.status(400).json({ error: 'Invalid URL' });
         }
 
         const post = await createShortUrl(data);
 
-        res.status(201).json(post);
+        return res.status(201).json(post);
     } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
         console.log(error);
+        return res.status(500).json({ error: 'Internal server error' });
     }
 };
