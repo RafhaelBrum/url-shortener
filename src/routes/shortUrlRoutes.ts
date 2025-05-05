aba2f82ea468cd37a51708c069723ba062ee7270
Rafhael <rafhael.brum@gmail.com>
2025-05-05 16:07:37 -0300

    add routes

--- src/routes/shortUrlRoutes.ts
+++ src/routes/shortUrlRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createShortUrlHandler } from "../controllers/shortUrlHandler";
+import { createShortUrlHandler, deleteShortUrlHandler, getOriginalUrlHandler, updateShortUrlHandler } from "../controllers/shortUrlHandler";
 
 const shortUrlRouter = Router();
 
 shortUrlRouter.post('/', createShortUrlHandler);
+shortUrlRouter.get('/:shortcode', getOriginalUrlHandler);
+shortUrlRouter.put('/:shortcode', updateShortUrlHandler);
+shortUrlRouter.delete('/:shortcode', deleteShortUrlHandler);
+shortUrlRouter.get('/:shortcode/stats', getOriginalUrlHandler);
 
 export default shortUrlRouter;
