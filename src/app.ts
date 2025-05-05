26a29b75278e1e290b646c1694038a430ac03738
Rafhael <rafhael.brum@gmail.com>
2025-05-05 14:51:01 -0300

    add url redirect

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import shortUrlRouter from './routes/shortUrlRoutes';
+import { redirectUrl } from './controllers/redirectHandler';
 
 const app = express();
 
 app.use(express.json());
 
-// app.use('/api', router)
+app.use('/shorten', shortUrlRouter);
+app.use('/:shortcode', redirectUrl);
 
 export default app;
