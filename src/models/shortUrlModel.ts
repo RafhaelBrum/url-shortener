0f02f486d16942d63f0f620319ee6b0195cd240b
Rafhael <rafhael.brum@gmail.com>
2025-05-05 15:27:41 -0300

    add UPDATE shortUrl model

--- src/models/shortUrlModel.ts
+++ src/models/shortUrlModel.ts
@@ -5,9 +5,9 @@ async function generateUniqueShortcode() {
     while (true) {
         const shortcode = Math.random().toString(36).substring(2, 8);
 
-        const query = 'SELECT * FROM urls WHERE short_code = $1;';
-        const values = [shortcode];
-        const result = await pool.query(query, values);
+        const QUERY = 'SELECT * FROM urls WHERE short_code = $1;';
+        const VALUES = [shortcode];
+        const result = await pool.query(QUERY, VALUES);
 
         if (result.rowCount === 0) return shortcode;
     }
@@ -18,9 +18,9 @@ export async function createShortUrl(data: shortUrlInput) {
     const url = data.url;
     const shortcode = await generateUniqueShortcode();
     try {
-        const INSERT = `INSERT INTO urls (url, short_code, created_at, updated_at) VALUES ($1, $2, $3, $4) RETURNING *;`;
+        const QUERY = `INSERT INTO urls (url, short_code, created_at, updated_at) VALUES ($1, $2, $3, $4) RETURNING *;`;
         const VALUES = [url, shortcode, date, date];
-        const result = await pool.query(INSERT, VALUES);
+        const result = await pool.query(QUERY, VALUES);
 
         return result.rows[0];
     } catch (error) {
@@ -31,18 +31,33 @@ export async function createShortUrl(data: shortUrlInput) {
 
 export async function getOriginalUrl(shortcode: string): Promise<shortUrlInput | undefined> {
     try {
-        const QUERY = 'SELECT * FROM urls WHERE short_code = $1';
-        const VALUES = [shortcode];
+        const selectQUERY = 'SELECT * FROM urls WHERE short_code = $1;';
+        const updateQUERY = 'UPDATE urls SET access_count = access_count + 1 WHERE short_code = $1;';
+
+        const values = [shortcode];
+
+        const result = await pool.query(selectQUERY, values);
+        if (result.rowCount === 0) return undefined;
+
+        await pool.query(updateQUERY, values);
+
+        return result.rows[0];
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function updateShortUrl(shortcode: string, newUrl: string) {
+    const date = new Date();
+    try {
+        const QUERY = `UPDATE urls SET url = $1, updated_at = $2 WHERE short_code = $3 RETURNING *;`;
+        const VALUES = [newUrl, date, shortcode];
         const result = await pool.query(QUERY, VALUES);
 
         return result.rows[0];
     } catch (error) {
         console.log(error);
     }
-};
-
-export async function updateShortUrl(shortcode: string, newUrl: string) {
-
 };
 
 export async function deleteShortUrl(shortcode: string) {
