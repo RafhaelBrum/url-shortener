6e4e1bb41b28f904321d9affb33f6138f4be7dcc
Rafhael <rafhael.brum@gmail.com>
2025-05-05 16:00:12 -0300

    add GET shortUrlStats handler

--- src/controllers/shortUrlHandler.ts
+++ src/controllers/shortUrlHandler.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createShortUrl, deleteShortUrl, getOriginalUrl, updateShortUrl } from "../models/shortUrlModel";
+import { createShortUrl, deleteShortUrl, getOriginalUrl, getShortUrlStats, updateShortUrl } from "../models/shortUrlModel";
 import { shortUrlInput } from "../types/shortUrl";
 import validator from 'validator';
 
@@ -84,7 +84,28 @@ export async function deleteShortUrlHandler(req: Request, res: Response) {
         res.status(204).send();
 
     } catch (error) {
-        res.status(500).json({ error: 'Internal serevr error' });
+        res.status(500).json({ error: 'Internal server error' });
     }
 
+};
+
+export async function getShortUrlStatsHandler(req: Request, res: Response) {
+    try {
+        const shortcode = req.params.shortcode;
+        if (!shortcode) {
+            res.status(400).json({ error: 'Bad request' });
+            return;
+        }
+
+        const data = await getShortUrlStats(shortcode);
+        if (!data) {
+            res.status(404).json({ error: 'Shortcode not found' });
+            return;
+        }
+
+        res.status(200).json(data);
+
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 };
